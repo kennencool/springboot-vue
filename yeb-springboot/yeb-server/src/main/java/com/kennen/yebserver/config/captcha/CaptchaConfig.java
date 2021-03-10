3f5f961f1efa346ad4df44c12aaae38b3260202c
kennencool <3304192108@qq.com>
2021-03-10 18:43:50 +0800

    配置vue.config.js实现跨域，前后端实å现通信

---
+++ yeb-springboot/yeb-server/src/main/java/com/kennen/yebserver/config/captcha/CaptchaConfig.java
@@ -0,0 +1,7 @@
+package com.kennen.yebserver.config.captcha;/**
+ * @Author: hejiyuan
+ * @Date: 2021/3/10 16:21
+ * @Description: 
+ *
+ */ public class CaptchaConfig {
+}
---
+++ yeb-springboot/yeb-server/src/main/java/com/kennen/yebserver/controller/login/CaptchaController.java
@@ -0,0 +1,7 @@
+package com.kennen.yebserver.controller.login;/**
+ * @Author: hejiyuan
+ * @Date: 2021/3/10 17:43
+ * @Description: 
+ *
+ */ public class CaptchaController {
+}
